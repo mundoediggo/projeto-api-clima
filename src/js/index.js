5cf6e7e405c4f1e7c2447053a4a28c22181dcd00
Diego Souza <contatodiegoasousa@gmail.com>
2024-12-13 18:40:14 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+const chaveDeApi = "9c7dd2ac5e78477ba25193614240711";
+const botaoDeBusca = document.querySelector(".btn-busca")
+
+botaoDeBusca.addEventListener("click", async () => {
+    const cidade = document.getElementById("input-busca").value;
+
+    if (!cidade) return;
+
+    const dados = await buscarDadosDeClima(cidade);
+
+    preencherDadosNaTela(dados, cidade);
+})
+
+async function buscarDadosDeClima(cidade) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=9c7dd2ac5e78477ba25193614240711&q=London&aqi=no&lang=pt`;
+
+    const resposta = await fetch(apiUrl);
+
+    if (resposta.status !== 200) return;
+
+    const dados = await resposta.json();
+    return dados;
+}
+
+function preencherDadosNaTela(dados, cidade) {
+    const temperatura = Math.round(dados.current.temp_c);
+    const umidade = dados.current.humidity;
+    const condicao = dados.current.condition.text;
+    const velocidadeDoVento = dados.current.wind_kph;
+    const iconeCondicao = dados.current.condition.icon;
+
+    document.getElementById("cidade").textContent = cidade;
+
+    document.getElementById("temperatura").textContent = `${temperatura} °C`;
+    document.getElementById("umidade").textContent = `${umidade} %`;
+    document.getElementById("condicao").textContent = condicao;
+    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento} km/h`;
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao)
+}
